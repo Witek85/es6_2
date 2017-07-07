5790c09ebd901a2d2b0d822cd74623577f1186d4
Witek85 <witold.szklarski@gmail.com>
2017-07-07 21:58:32 +0200

    class

--- js/scripts-compiled.js
+++ js/scripts-compiled.js
@@ -1,5 +1,22 @@
 'use strict';
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// class ma hoisting tak jak var function
+var Article = function Article() {
+  _classCallCheck(this, Article);
+};
+
+var Article2 = function ArticleClass() {
+  _classCallCheck(this, ArticleClass);
+};
+
+var art1 = new Article();
+var art2 = new Article2();
+
+console.log(art1);
+console.log(art2);
+
 window.onload = function () {
 
   var w = 'works';
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
+// class ma hoisting tak jak var function
+class Article {
+
+}
+
+const Article2 = class ArticleClass {
+
+}
+
+const art1 = new Article;
+const art2 = new Article2;
+
+console.log(art1);
+console.log(art2);
+
 window.onload = function() {
 
   let w = 'works';
