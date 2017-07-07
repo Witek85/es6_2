a25c086fa4b41e4e9a47f38f8f831b9e09abab08
Witek85 <witold.szklarski@gmail.com>
2017-07-07 22:09:50 +0200

    konstruktor

--- js/scripts-compiled.js
+++ js/scripts-compiled.js
@@ -1,21 +1,26 @@
-'use strict';
+"use strict";
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 // class ma hoisting tak jak var function
-var Article = function Article() {
+var Article = function Article(id, title) {
   _classCallCheck(this, Article);
+
+  this.id = id;
+  this.title = title;
 };
 
 var Article2 = function ArticleClass() {
   _classCallCheck(this, ArticleClass);
 };
 
-var art1 = new Article();
+var art1 = new Article(123, "First title");
 var art2 = new Article2();
 
-console.log(art1);
 console.log(art2);
+console.log(art1);
+console.log(art1.id);
+console.log(art1.title);
 
 window.onload = function () {
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 // class ma hoisting tak jak var function
 class Article {
+  constructor(id, title) {
+    this.id = id;
+    this.title = title;
+  }
 
 }
 
@@ -7,11 +11,13 @@ const Article2 = class ArticleClass {
 
 }
 
-const art1 = new Article;
+const art1 = new Article(123, "First title");
 const art2 = new Article2;
 
-console.log(art1);
 console.log(art2);
+console.log(art1);
+console.log(art1.id);
+console.log(art1.title);
 
 window.onload = function() {
 
