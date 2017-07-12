f24708cc920aba0477386ae6e1e9203929eafa8d
Witek85 <witold.szklarski@gmail.com>
2017-07-12 22:39:27 +0200

    math

--- js/bundle.js
+++ js/bundle.js
@@ -129,6 +129,16 @@ var typedArr2 = new Float32Array([1, 4, 6, 9.394, 3000]);
 console.log(typedArr1);
 console.log(typedArr2);
 
+// Math
+
+var num1 = -21.54;
+console.log(Math.sign(num1));
+console.log(Math.trunc(num1));
+var num2 = 8;
+console.log(Math.log2(num2));
+var num3 = 100;
+console.log(Math.log10(num3));
+
 /***/ }),
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
--- js/main.js
+++ js/main.js
@@ -50,3 +50,13 @@ const typedArr1 = new Uint8Array([1,4,6,9.394,3000]);
 const typedArr2 = new Float32Array([1,4,6,9.394,3000]);
 console.log(typedArr1); 
 console.log(typedArr2); 
+
+// Math
+
+let num1 = -21.54;
+console.log(Math.sign(num1));
+console.log(Math.trunc(num1));
+let num2 = 8;
+console.log(Math.log2(num2));
+let num3 = 100;
+console.log(Math.log10(num3));
